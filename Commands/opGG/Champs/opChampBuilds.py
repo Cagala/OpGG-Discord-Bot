c7cb3bd78c07888599b5a980620a3c1a5f17a8d2
Cagala <115490518+Cagala@users.noreply.github.com>
2022-10-13 19:25:54 +0300

    Update opChampBuilds.py

--- Commands/opGG/Champs/opChampBuilds.py
+++ Commands/opGG/Champs/opChampBuilds.py
@@ -16,7 +16,7 @@ class opChampBuilds:
         self.champName = champName
         self.position = position
 
-        self.buildsApi = f"https://www.op.gg/_next/data/{_NEXT}/champions/{self.champName}/top/build.json?region=global&tier=diamond&champion={self.champName}&position={self.position}&hl=TR"
+        self.buildsApi = f"https://www.op.gg/_next/data/{_NEXT}/champions/{self.champName}/top/build.json?region=global&tier=diamond&champion={self.champName}&position={self.position}"
         self.headers = headers if headers else {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.102 Safari/537.36 OPR/90.0.4480.54", "accept" : "application/json"}
         self.ctx = ctx
         self.ID = selfRequsestID
